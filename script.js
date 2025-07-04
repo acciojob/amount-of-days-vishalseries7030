d7c2d18a72adff66eceef0fad29c00f036097a0f
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-07-04 19:51:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-//your JS code here. If required.
-function daysofAYear(year){
-	if((year % 4 === 0 && year % 100  !== 0) || (year % 400 === 0)){
-		return 366;
-	}else{
-		return 365;
-	}
+function daysOfAYear(year) {
+  // Leap year logic
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366;
+  } else {
+    return 365;
+  }
 }
 
-console.log(daysOfAYear(2022)); 
-console.log(daysOfAYear(2024)); 
-console.log(daysOfAYear(1900)); 
-console.log(daysOfAYear(2000));
+let input = prompt("Enter a year:");
+let year = parseInt(input);
+
+alert(daysOfAYear(year));
+
+
