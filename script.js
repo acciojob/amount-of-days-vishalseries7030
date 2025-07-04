5cd3323a73627a858fddb6e359d7ee97ecabb370
vishal payyawar <139535850+vishalseries7030@users.noreply.github.com>
2025-07-04 19:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysofAYear(year){
+	if((year % 4 === 0 && year % 100  !== 0) || (year % 400 === 0)){
+		return 366;
+	}else{
+		return 365;
+	}
+}
+
+console.log(daysOfAYear(2022)); 
+console.log(daysOfAYear(2024)); 
+console.log(daysOfAYear(1900)); 
+console.log(daysOfAYear(2000));
